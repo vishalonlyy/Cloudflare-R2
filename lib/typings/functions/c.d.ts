ccb2986ad5431059957d38234edc4a100834bdd0
Vishal <vishalrajpurohit416@gmail.com>
2024-01-18 19:16:55 +0530

    Update dependencies and add new files supporting typescript

--- lib/typings/functions/c.d.ts
+++
@@ -1,5 +0,0 @@
-import { S3Client } from "@aws-sdk/client-s3";
-declare let S3Client__: S3Client;
-declare function setS3(data: S3Client): Promise<void>;
-declare function getS3(): Promise<S3Client>;
-export { S3Client__, setS3, getS3 };
